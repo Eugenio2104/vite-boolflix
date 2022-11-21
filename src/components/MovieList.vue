f95e65c3a8a25ea12089859b3bd23ba0396003aa
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-21 19:00:54 +0100

    creazione nuove card con errore

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -10,72 +10,81 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="flip-card text-center">
-        <div class="flip-card-inner">
-          <div class="flip-card-front">
-            <img
-              :src="'http://image.tmdb.org/t/p/w342/' + movie.poster_path"
-              :alt="movie.title"
-              style="width: 300px; height: 300px"
-            />
-          </div>
-          <div class="flip-card-back">
-            <!-- titolo, titolo originale, lingua, voto -->
-            <h1>{{ movie.title }}</h1>
-            <p>{{ movie.original_title }}</p>
-            <p>{{ movie.vote_averange }}</p>
-            <p>{{ movie.original_language }}</p>
-            <p>{{ movie.overview }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="card">
+    <img
+      :src="'http://image.tmdb.org/t/p/w342/' + movie.poster_path"
+      :alt="movie.title"
+    />
+    <h1>{{ movie.title }}</h1>
+    <p>
+      Lorem Ipsum is simply dummy text from the printing and typeseting industry
+    </p>
   </div>
 </template>
 
 
 
 <style lang="scss" scoped>
-.flip-card {
-  background-color: transparent;
-  width: 300px;
-  height: 300px;
-  perspective: 1000px;
-}
-
-.flip-card-inner {
+.card {
+  display: flex;
+  width: calc(100% / 6);
+  margin: 20px;
+  height: 360px;
+  border-radius: 15px;
+  padding: 1.5rem;
+  background: white;
   position: relative;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  transition: transform 0.6s;
-  transform-style: preserve-3d;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.4s ease-out;
+  box-shadow: 0px 7px 10px rgba(0, 0, 0, 0.5);
 }
-
-.flip-card:hover .flip-card-inner {
-  transform: rotateY(180deg);
+.card:hover {
+  transform: translateY(20px);
 }
-
-.flip-card-front,
-.flip-card-back {
+.card:hover:before {
+  opacity: 1;
+}
+.card:hover .info {
+  opacity: 1;
+  transform: translateY(0px);
+}
+.card:before {
+  content: "";
   position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
   width: 100%;
   height: 100%;
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
+  border-radius: 15px;
+  background: rgba(0, 0, 0, 0.6);
+  z-index: 2;
+  transition: 0.5s;
+  opacity: 0;
 }
-
-.flip-card-front {
-  background-color: #bbb;
-  color: black;
+.card img {
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-radius: 15px;
 }
-
-.flip-card-back {
-  background-color: #2980b9;
+.card .info {
+  position: relative;
+  z-index: 3;
   color: white;
-  transform: rotateY(180deg);
+  opacity: 0;
+  transform: translateY(30px);
+  transition: 0.5s;
+}
+.card .info h1 {
+  margin: 0px;
+}
+.card .info p {
+  letter-spacing: 1px;
+  font-size: 15px;
+  margin-top: 8px;
 }
 </style>
