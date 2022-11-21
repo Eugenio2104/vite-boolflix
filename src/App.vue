d282a2b06718ce5efd77ef920c68b28d2b8cc6c0
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-21 12:26:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppCard from "./components/AppCard.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppCard,
+    AppFooter,
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <main>
+    <AppCard />
+    <AppFooter />
+  </main>
+</template>
+
+
+<style lang="scss">
+@use "./styles/general";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <h1>Boolflix</h1>
+    </div>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+.container {
+  h1 {
+    text-transform: uppercase;
+  }
+}
+</style>
